734f75dc8b6ce0d392b690e4f920f927c9097183
iiTzJacob <jj.colon7802@gmail.com>
2022-12-23 18:49:16 -0500

    Finished functions

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
-const container = document.getElementById('grid');
+function makeSquare(side) {
+    const total = side * side;
+    const html = [];
+    for (let i = 0; i < total; i++) {
+        html.push('<div class="square"></div>');
+    }
+    return html.join('');
+}
 
-function makeRows(rows, columns) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', columns);
-    for (i = 0; i < (rows * columns); i++) {
-        let cell = document.createElement('div');
-        cell.innerText = (i + 1); 
-        container.appendChild(cell).className = "grid-item";
-    };
-};
+const container = document.querySelector('.container');
+container.innerHTML = makeSquare(16)
 
-makeRows(16,16);
+const squares = container.querySelectorAll('.square');
 
+function changeColor() {
+    this.classList.toggle('black');
+}
+
+squares.forEach(square => square.addEventListener('mouseover', changeColor));
+
+const btn = document.querySelector('.button');
+btn.addEventListener('click', changeLayout);
+
+function changeLayout() {
+    const size = parseInt(prompt("How many square would you like?"));
+    let squareSize = (960 / size) - 2;
+
+    document.documentElement.style.setProperty('--side-square', squareSize + 'px');
+    container.innerHTML = makeSquare(size);
+
+    const squares = container.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('mouseover', changeColor));
+}
